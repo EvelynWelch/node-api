0e44b8a77607011c3a1743a16a28563a543f4224
Evelyn Welch <evieswelch@gmail.com>
2024-02-04 00:50:15 -0700

    added tests for utils.Observer

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -5,32 +5,29 @@ const testNumbers = [0, 1, 2, 3, 4, 5]
 function queueFactory() {
     const queue = new Queue<Number>();
     testNumbers.forEach(n => queue.enqueue(n))
-    
+
     return queue
 }
 
-
-test('utils.Queue.enqueue()', () => {    
+test('utils.Queue.enqueue()', () => {
     const queue = queueFactory();
-    
+
     const items = queue.getItems()
 
-    for(let i = 0; i < items.length; i++){
+    for (let i = 0; i < items.length; i++) {
         expect(items[i]).toBe(testNumbers[i])
     }
 
 })
 
-
 test('utils.Queue.dequeue()', () => {
     const queue = queueFactory();
     const items = queue.getItems();
-    for(let i = 0; i < items.length; i++){
+    for (let i = 0; i < items.length; i++) {
         const deq = queue.dequeue()
         expect(deq).toBe(testNumbers[i])
     }
 })
-
 
 test("utils.Queue.size", () => {
     const queue = queueFactory();
@@ -44,3 +41,80 @@ test("utils.Queue.isEmpty", () => {
     expect(queue.isEmpty).toBe(false);
 
 })
+
+const testEvent = "test";
+function tf1() { return 1; }
+function tf2() { return 2; }
+function tf3() { return 3; }
+
+function obsFactory() {
+    const obs = new Observer();
+    const registerSuccess = obs.registerEvent(testEvent);
+    return obs;
+}
+describe("utils.Observer", () => {
+    test("registerEvent()", () => {
+        const obs = new Observer();
+        const registerSuccess = obs.registerEvent(testEvent);
+        expect(registerSuccess).toBeTruthy();
+        expect(obs.hasEvent(testEvent)).toBeTruthy();
+    });
+
+    test("unregisterEvent()", () => {
+        const obs = obsFactory();
+        const testRemove = obs.unregisterEvent(testEvent);
+        expect(testRemove).toBeTruthy();
+        expect(obs.hasEvent('test')).toBeFalsy();
+    });
+
+    test("subscribe()", () => {
+        const obs = obsFactory();
+        const subscribed = obs.subscribe(testEvent, () => { });
+        expect(subscribed).toBeTruthy();
+        const failedSubscribe = obs.subscribe('fubar', () => { });
+        expect(failedSubscribe).toBeFalsy();
+    });
+
+    test("unsubscribe()", () => {
+        const obs = obsFactory();
+        
+        let t1 = false;
+        let t2 = false;
+        let t3 = false;
+        function t1f() { t1 = true; }
+        function t2f() { t2 = true; }
+        function t3f() { t3 = true; }
+        obs.subscribe(testEvent, t1f)
+        obs.subscribe(testEvent, t2f)
+        obs.subscribe(testEvent, t3f)
+
+        const unsubscribed = obs.unsubscribe(testEvent, t2f);
+
+        const subs = obs.getSubs(testEvent);
+
+        expect(subs.length).toBe(2)
+        obs.fire(testEvent, null);
+        
+        expect(unsubscribed).toBeTruthy();
+        expect(t2).toBeFalsy();
+    });
+
+    test("fire()", () => {
+        const obs = obsFactory();
+        let t1 = false;
+        let t2 = false;
+        let t3 = false;
+        function t1f() { t1 = true; }
+        function t2f() { t2 = true; }
+        function t3f() { t3 = true; }
+        obs.subscribe(testEvent, t1f)
+        obs.subscribe(testEvent, t2f)
+        obs.subscribe(testEvent, t3f)
+
+        obs.fire(testEvent, null);
+
+        expect(t1).toBeTruthy();
+        expect(t2).toBeTruthy();
+        expect(t3).toBeTruthy();
+     });
+});
--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,8 @@
 export function getEnvironmentVariable(name: string): string {
     const value = process.env[name];
-    
-    const errorString = "utils.getEnviromentVariable() error: " +  name + " is not an in process.env[]"
-    if(!value) {
+
+    const errorString = "utils.getEnviromentVariable() error: " + name + " is not an in process.env[]"
+    if (!value) {
         console.error(errorString)
         throw new Error("utils.getEnviromentVariable() error: enviroment variable not found in process.env[]");
     }
@@ -12,10 +12,10 @@ export function getEnvironmentVariable(name: string): string {
 
 export class Queue<T> {
     private items: Array<T>;
-    constructor(){
+    constructor() {
         this.items = [];
     }
-    get size() : number {
+    get size(): number {
         return this.items.length
     }
     get isEmpty(): boolean {
@@ -30,7 +30,7 @@ export class Queue<T> {
     peek(): T {
         return this.items[0]
     }
-    
+
     getItems(): Array<T> {
         // const clone = this.items.map((x) => x)
         return this.items.map((x) => x)
@@ -43,42 +43,57 @@ export class Observer {
     constructor() {
         this.observers = new Map();
     }
-
-    registerEvent(event: string) {
-        if(!this.observers.has(event)){
+    getSubs(event: string): Array<Function> {
+        return this.observers.get(event).map((x) => x);
+    }
+    hasEvent(event: string): boolean {
+        return this.observers.has(event);
+    }
+    private updateSubscribers(event: string, subscribers: Array<Function>) {
+        if(!this.observers.has(event)) return false;
+        this.observers.set(event, subscribers);
+    }
+    registerEvent(event: string): boolean {
+        if (this.observers.has(event)) return false;
             this.observers.set(event, [])
-        }
+            return true;
     }
 
-    unregisterEvent(event: string){
-        if(this.observers.has(event)){
-            this.observers.delete(event)
-        }
+    unregisterEvent(event: string): boolean {
+        if (!this.observers.has(event)) return false;
+        this.observers.delete(event)
+        return true;
     }
 
-    subscribe(event: string, callback: Function ): void {
-        this.observers.get(event).push(callback)
+    subscribe(event: string, callback: Function): boolean {
+        if (!this.observers.has(event)) return false;   
+        this.observers.get(event).push(callback);
+        return true;
     }
 
-    unsubscribe(event: string, callback: Function): void {
-        if(!this.observers.has(event)){
-            return
-        } 
+    unsubscribe(event: string, callback: Function): boolean {
+        if (!this.observers.has(event)) { return false; }
         const subscribers = this.observers.get(event)
-        for(let i = 0; i < subscribers.length; i++){
-            if(subscribers[i] === callback){
-                const removed = subscribers.slice(i, i + 1)
-                return             }
-        }
+        let callbackRemoved = false;   
+        const updated = subscribers.filter((elem) => {
+            if (elem === callback) {
+                callbackRemoved = true;   
+                return false;
+            }
+            return true;
+        })
+        this.updateSubscribers(event, updated);
+        return callbackRemoved
     }
 
     fire(event: string, args: any) {
+        if (!this.observers.has(event)) return;
         const subscribers = this.observers.get(event)
-        // let returnArgs = args
+        let returnArgs = args
         subscribers.forEach((callback) => {
-            callback(args)
+           returnArgs = callback(args)
         })
-        return args
+        return returnArgs
     }
 }
 
